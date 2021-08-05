812a639e9e961ddaf34c9d8874d8c64adaad1b73
cAARA <gomney@gmail.com>
2021-08-05 05:42:23 -0500

    Overall skeleton put in place

--- main.py
+++ main.py
@@ -6,25 +6,39 @@ im = np.array(Image.open('early-snow.png').convert('RGBA').rotate(90, expand=1))
 
 minLength = 15
 
-def sortInterval(input,begin,end):
+#Sorts based on red channel only. Shockingly good
+def sortDefault(pixel):
+    return pixel
+    
 
-    interval = np.empty((end-begin, 4))
-    #print('begining interval from ', begin, ' to ', end)
+#Listing of sorting methods
+SORTMETHODS = {
 
-    for i, pixel in enumerate(input):
-        if i == end:
-            break
-        if i >= begin:
-            interval[i-begin] = pixel
+    "DEFAULT" : sortDefault
+
+}
+
+
+def sortIntervals(inputImage,inputIntervals,method):
+
+    print("beginning sorting....")
+    #Create Parsed version of image for determination of sorting 
+    parsedImage = inputImage.copy(order='K')
+
+    for line in parsedImage:
+        for pixel in line:
+            #Parse each pixel based on chosen method
+            pixel = SORTMETHODS[method](pixel)
+
+    for i, intLine in enumerate(inputIntervals):
+        #For every line, sort the intervals previously decided upon
+        for j, interval in enumerate(intLine):
+            sort = np.argsort(parsedImage[i, interval[0]:interval[1]], axis=0)
+            inputImage[i, interval[0]:interval[1]] = np.take_along_axis(inputImage[i, interval[0]:interval[1]], sort, 0)
         
 
-    interval.sort(0)
 
-    for i, pixel in enumerate(interval):
-        input[begin+i] = pixel
-
-
-#Implementations of interval selection methods
+#Finds intervals based on channel diff w/ minimum size
 def intervalMinSize(input):
 
     allIntervals = []
@@ -34,18 +48,20 @@ def intervalMinSize(input):
         currentInterval = [0,0]
         allIntervals.append([])        
         
+        print("finding intervals on line" + str(i))
         for currentInterval[1], pixel in enumerate(line):
-               #compare current pixel to the pixel at the start of the interval
-               if abs(line[currentInterval[0]] - pixel)[3] > 10 or abs(line[currentInterval[0]] - pixel)[0] > 90:
+
+            #compare current pixel to the pixel at the start of the interval
+           if abs(line[currentInterval[0]] - pixel)[3] > 10 or abs(line[currentInterval[0]] - pixel)[0] > 90:
                     
-                    #print([currentInterval[0],currentInterval[1]])
-                    #only adds to list if the length is more than minLength; always resets new interval location
-                    #Ensures high contrast areas stay readable
-                    if currentInterval[1] - currentInterval[0] > minLength:
+                #print([currentInterval[0],currentInterval[1]])
+                #only adds to list if the length is more than minLength; always resets new interval location
+                #Ensures high contrast areas stay readable
+                if currentInterval[1] - currentInterval[0] > minLength:
                         
-                        #print("added")
-                        allIntervals[i].append(currentInterval[:])
-                    currentInterval[0] = currentInterval[1] + 1
+                    #print("added")
+                    allIntervals[i].append(currentInterval[:])
+                currentInterval[0] = currentInterval[1] + 1
          
         if currentInterval[0] < line.shape[0]:
             
@@ -64,37 +80,17 @@ INTERVALMETHODS = {
 }
 
 
-###Finds list of intervals to be sorted
+###Selects and executes interval method
 def findIntervals(input, method):
 
-    intervalMinSize(input)
+    print("finding intervals...")
+    output = INTERVALMETHODS[method](input)
+    return output
 
-def   
-
-
+  
 ######BEGIN MAIN FUNCTION######
 intervals = findIntervals(im,"MINSIZE")
-
-
-#for i, line in enumerate(im):
-#
-#    #begin interval at the start of the line
-#    intervalStart = 0
-#    print('starting line: ' + str(i))#
-#
-#    for j, pixel in enumerate(line):
-#        #compare current pixel to the pixel at start of interval
-#        if abs(line[intervalStart] - pixel)[3] > 10 or abs(line[intervalStart] - pixel)[0] > 90:
-#            
-#            #Only sorts if length more than minLength; always resets new interval location
-#            #This ensures low contrast areas stay readable
-#            if j - intervalStart > minLength:
-#                sortInterval(line, intervalStart, j)
-#            intervalStart = j + 1
-#    
-#    #Sort the last interval once the line is over
-#    if intervalStart < line.shape[0]:
-#        sortInterval(line, intervalStart, line.shape[0] -1)
+sortIntervals(im, intervals, "DEFAULT")
 
 #Write to output
 Image.fromarray(im).rotate(270, expand=1).save('early-snow-tim.png')
