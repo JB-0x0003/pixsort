48f1174543897c0ab64afed13aba8a982401ca08
cAARA <gomney@gmail.com>
2021-08-17 02:57:02 -0500

    new sorting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import numpy as np
-from PIL import Image
-import sys
+from PIL import Image, ImageFilter
 import argparse
 
 #Sorts based on red channel only. Surprisingly good
@@ -26,6 +25,7 @@ def sortMedian(input):
             pixel[0] = abs(medianPixelR - pixel[0])
             pixel[1] = pixel[0]
             pixel[2] = pixel[0]
+            
     return input
 
 #Listing of sorting methods
@@ -70,10 +70,8 @@ def intervalDiff(input):
                 #print([currentInterval[0],currentInterval[1]])
                 #only adds to list if the length is more than minLength; always resets new interval location
                 #Ensures high contrast areas stay readable
-                if currentInterval[1] - currentInterval[0] > minLength:
-                        
-                    #print("added")
-                    allIntervals[i].append(currentInterval[:])
+                
+                allIntervals[i].append(currentInterval[:])
                 currentInterval[0] = currentInterval[1] + 1
          
         if currentInterval[0] < line.shape[0]:
@@ -86,12 +84,17 @@ def intervalDiff(input):
 
     return allIntervals
 
+def intervalEdge(Input):
+    pass
+
 
 #List of interval selection methods
 INTERVALMETHODS = {
-    "DIFFERENCE" : intervalDiff
-}
 
+    "DIFFERENCE" : intervalDiff,
+    "EDGE" : intervalEdge
+
+}
 
 ###Selects and executes interval method
 def findIntervals(input, method):
@@ -100,24 +103,63 @@ def findIntervals(input, method):
     output = INTERVALMETHODS[method](input)
     return output
 
-  
+#Default Values
+DEFAULTS = {
+
+    "IntervalMethod" : "DIFFERENCE",
+    "SortMethod" : "MEDIAN",
+    "RotDegrees" : 0
+
+}
+
 ######BEGIN MAIN FUNCTION######
-#Begin parsing argumnets
+#Begin parsing arguments
 parser = argparse.ArgumentParser(description="Easily Smear Pixels")
 parser.add_argument("image", help="File path of the image to be sorted")
 parser.add_argument("-o", "--output", help="File path for output to be saved to")
+parser.add_argument("-i", "--interval", help="Method for determining intervals")
+parser.add_argument("-s", "--sorter", help="Method for sorting pixels in intervals")
+parser.add_argument("-m", "--mask", help="Mask to exclude areas of image from sorting")
+parser.add_argument("-r", "--rotate", help="Degrees of rotation for each pixel sorting pass. Default is horizontal left to right")
 
+#Pop and save arguments from arg parser
 args = parser.parse_args()
 inputPath = args.image
 outputPath = args.output
+intervalMethod = args.interval
+sortMethod = args.sorter
+mask = args.mask
+rotDegrees = int(args.rotate)
 
-im = np.array(Image.open(inputPath).convert('RGBA').rotate(90, expand=1))
+#apply defaults for necessary arguments which are unspecified
+if intervalMethod == None:
+    intervalMethod = DEFAULTS["IntervalMethod"]
+if sortMethod == None:
+    sortMethod = DEFAULTS["SortMethod"]
+if rotDegrees == None:
+    rotDegrees = DEFAULTS["RotDegrees"]
 
-minLength = 5
+im = Image.open(inputPath).convert('RGBA').rotate(rotDegrees, expand=1)
+
+#edges = im.convert('L').filter(ImageFilter.FIND_EDGES).filter(ImageFilter.FIND_EDGES).filter(ImageFilter.BoxBlur(7))
+#edges = np.array(edges)
+
+#threshold = 60#
+
+#for i, line in enumerate(edges):
+ #   for j, pixel in enumerate(line):
+  #      if edges[i,j] > threshold:
+   #         edges[i,j] = 255
+    #    else:
+     #       edges[i,j] = 0
+        
+        
 
 
-#intervals = findIntervals(im,"DIFFERENCE")
-#sortIntervals(im, intervals, "MEDIAN")
+im = np.array(im)
+
+intervals = findIntervals(im,intervalMethod)
+sortIntervals(im, intervals, sortMethod)
 
 #Determine output path if none available
 if outputPath == None:
@@ -130,4 +172,4 @@ if outputPath == None:
 
 
 #Write to output
-Image.fromarray(im).rotate(270, expand=1).save(outputPath)
+Image.fromarray(im).rotate(360-rotDegrees, expand=1).save(outputPath)
